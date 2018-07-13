a91c677898f5e51a03331b4fb65d6efd1b628c82
Paul Herbert <paul.herbert@stratisplatform.com>
2018-07-13 12:42:43 +0100

    Initial commit of Statistics work

---
+++ src/Stratis.Bitcoin/Statistics/Interfaces/IStatistic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stratis.Bitcoin.Statistics.Interfaces
+{
+    public interface IStatistic
+    {
+        string Name { get; }
+
+        string Value { get; }
+
+        IEnumerable<IStatistic> Statistics { get; }
+    }
+}
---
+++ src/Stratis.Bitcoin/Statistics/Statistic.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stratis.Bitcoin.Statistics
+{
+    class Statistic
+    {
+    }
+}
